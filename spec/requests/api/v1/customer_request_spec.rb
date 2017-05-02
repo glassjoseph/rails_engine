300d28f6216074b1bbc489f2bb2177802d694c57
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-02 16:40:56 -0600

    add customer endpoint
    *add customer API controller
    *add customer tests
    *add customer routes

---
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Customers API" do
+  it "shows all customers" do
+    create_list(:customer, 3)
+    db_customer = Customer.first
+
+    get "/api/v1/customers"
+
+    customers = JSON.parse(response.body)
+    customer = Customer.first
+
+    expect(response).to be_success
+    expect(customer["id"]).to eq(1)
+    expect(customer["first_name"]).to eq(db_customer.first_name)
+    expect(customer["last_name"]).to eq(db_customer.last_name)
+  end
+
+
+    it "shows one customer" do
+      db_customer = create(:customer)
+      get "/api/v1/customers/#{db_customer.id}"
+
+      expect(response).to be_success
+
+      customer = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(customer["id"]).to eq(4)
+      expect(customer["first_name"]).to eq(db_customer.first_name)
+      expect(customer["last_name"]).to eq(db_customer.last_name)
+    end
+end

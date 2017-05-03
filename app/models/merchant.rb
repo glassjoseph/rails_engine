f52d42071c7325f0e64f4305bdd885e661f94074
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-02 20:04:39 -0600

    add merchant revenue analytics
    *add test for merchant
    *add revenue controller
    *add routes with custom revenue route

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def revenue
+    sum = invoices.joins(:invoice_items).sum("unit_price * quantity")
+    {revenue: sum}
+  end
 end

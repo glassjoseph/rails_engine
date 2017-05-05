d2f471100c54865b1064c9b8caa9c548988c0d50
glassjoseph <glassjoseph@gmail.com>
2017-05-04 22:50:02 -0600

    add most_items class method

--- spec/requests/api/v1/items/items_bi_request_spec.rb
+++ spec/requests/api/v1/items/items_bi_request_spec.rb
@@ -21,4 +21,30 @@ RSpec.describe 'Item Business Intelligence API' do
       expect(response).to be_success
       expect(date["best_day"]).to eq("2012-03-25T13:54:11.000Z")
     end
+
+  it 'returns top items by number sold' do
+      create_list(:item, 5)
+      create_list(:invoice, 5)
+      item1 = Item.first
+      item2 = Item.second
+
+      Invoice.all do |inv|
+        inv.transactions << create(:transaction)
+      end
+
+      Invoice.first.invoice_items << create(:invoice_item, quantity: 99)
+
+      item1.invoices << Invoice.first
+      item2.invoices << Invoice.last
+
+
+
+
+      get "/api/v1/items/most_items?quantity=1"
+
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items[0]).to eq(item1[0])
+    end
   end

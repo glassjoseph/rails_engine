65c8890dd0cee01b92d25ca5f88c22a5c023cad4
glassjoseph <glassjoseph@gmail.com>
2017-05-05 08:17:41 -0600

    fixed prices in items, invoice_items

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Items::FindController < ApplicationController
 
   def index
+    params[:unit_price].gsub!('.', '') if params[:unit_price]
     render json: Item.where(search_params)
   end
 
 
   def show
+    params[:unit_price].gsub!('.', '') if params[:unit_price]
     render json: Item.find_by(search_params)
   end
 
